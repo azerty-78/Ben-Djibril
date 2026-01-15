2f94be34897b043c329114e12fe6d8cf2ee99892
Ben Djibril <konedjibril2004@gmail.com>
2026-01-15 11:40:31 +0100

    refactor: comment out unused components in Home page for cleaner code and future flexibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import HomeHero from '../components/home/HomeHero'
 import HomeAbout from '../components/home/HomeAbout'
 import HomeCertifications from '../components/home/HomeCertifications'
 import HomeServices from '../components/home/HomeServices'
-import HomeMicroTestimonial from '../components/home/HomeMicroTestimonial'
-import HomeTestimonials from '../components/home/HomeTestimonials'
+// import HomeMicroTestimonial from '../components/home/HomeMicroTestimonial'
+// import HomeTestimonials from '../components/home/HomeTestimonials'
 import HomeCTA from '../components/home/HomeCTA'
 
 function Home() {
@@ -15,8 +15,8 @@ function Home() {
       <HomeAbout />
       <HomeCertifications />
       <HomeServices />
-      <HomeMicroTestimonial />
-      <HomeTestimonials />
+      {/* <HomeMicroTestimonial /> */}
+      {/* <HomeTestimonials /> */}
       <HomeCTA />
     </div>
   )
